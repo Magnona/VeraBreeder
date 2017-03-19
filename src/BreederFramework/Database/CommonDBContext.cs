9ce9bd893d0f4094ea30086ef05c47cccbf20f30
Elena Shauliuk <eshauliuk@e4d.com>
2017-03-18 23:56:34 -0500

    init repository

---
+++ src/BreederFramework/Database/CommonDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BreederFramework.Database
+{
+    internal abstract class CommonDBContext : DbContext
+    {
+
+        private readonly string _tablePrefix;
+    }
+}
---
+++ src/BreederFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BreederFramework")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d765b9a6-a243-4666-aca2-d4359037ddd3")]
